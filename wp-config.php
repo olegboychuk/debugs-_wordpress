451bee0be510a90b93cc1e165f16113e589dc786
olegboychuk <oleg.boychuk@gmail.com>
2019-07-10 10:00:50 +0300

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,9 @@
+// define('WP_DEBUG', true);
+// Turns WordPress debugging on
+define('WP_DEBUG', true);
+// Tells WordPress to log everything to the /wp-content/debug.log file
+define('WP_DEBUG_LOG', true);
+// Doesn't force the PHP 'display_errors' variable to be on
+define('WP_DEBUG_DISPLAY', false);
+//Hides errors from being displayed on-screen
+@ini_set('display_errors', 0);
